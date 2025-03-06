c008231adc0a44be4e3a51df5a629829fea8a921
Hector-T0 <hect853@gmail.com>
2025-03-06 17:03:08 -0500

    made the habits table

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -12,3 +12,11 @@ export const users = pgTable('users', {
 
 export type User = typeof users.$inferSelect;     //inferSelect helps in using SELECT queries for database. 
 export type NewUser = typeof users.$inferInsert;  //helps in inserting data into database & omits primary key or optional columns
+
+
+export const habits = pgTable('habits', {
+    hab_id: uuid().primaryKey().defaultRandom(),
+    owner: uuid().notNull().references(() => users.user_id), // Foreign key referencing users
+    content: varchar({ length: 255 }), // Text content for the habit
+    image: varchar({ length: 255 })
+});
