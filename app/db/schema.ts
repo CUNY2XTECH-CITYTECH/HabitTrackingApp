e17c380e6b280f2c43e82da8490f711b38d8fda5
Hector-T0 <hect853@gmail.com>
2025-03-11 13:11:07 -0400

    updated table

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -15,8 +15,8 @@ export type NewUser = typeof users.$inferInsert;  //helps in inserting data into
 
 
 export const habits = pgTable('habits', {
-    hab_id: uuid().primaryKey().defaultRandom(),
-    owner: uuid().notNull().references(() => users.user_id), // Foreign key referencing users
+    id: uuid().primaryKey().defaultRandom(),
+    owner_id: uuid().notNull().references(() => users.user_id), // Foreign key referencing users
     content: varchar({ length: 255 }), // Text content for the habit
-    image: varchar({ length: 255 })
+    image_url: varchar({ length: 255 })
 });
