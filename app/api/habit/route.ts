2ff2672d57e59ea9df15c3ad16c366f115805881
LakpaSherpa123 <iamlakpa2053@gmail.com>
2025-03-12 16:26:18 -0400

    Adjusted the GET Api request according to the updated scheme

--- app/api/habit/route.ts
+++ app/api/habit/route.ts
@@ -1,18 +1,26 @@
 import {db} from '@/app/db/index'
 import { eq } from 'drizzle-orm';
-import {posts, NewPosts, Posts} from '@/app/db/schema'
-import { NextResponse } from 'next/server';
+import {habits, NewHabits, Habits} from '@/app/db/schema'
+
 
 //need to handle all the end cases like null or no payload.
 export const GET = async(request:Request)=> {
-
+   
     const retrieveUrl = await new URL(request.url);
     
-    const posts_id = retrieveUrl.searchParams.get("posts_id");
+    const habit_id = retrieveUrl.searchParams.get("habit_id");
   
-    const habit = await db.select({posts_details: posts.posts_details})
-                                    .from(posts)
-                                    .where(eq(posts.posts_id, posts_id!))           //posts_id! becuase next js thinks it could be null
+    const habit = await db.select({content: habits.content})
+                                    .from(habits)
+                                    .where(eq(habits.id, habit_id!));         //posts_id! because next js thinks it could be null
 
-    return Response.json(habit); 
+    return Response.json(habit);
+
+}
+
+
+export const POST = async(request:Request)=>{
+
+
+
 }
