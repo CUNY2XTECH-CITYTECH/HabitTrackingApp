45a6f7760250d37e50185e88a51d416922af1845
Hector-T0 <hect853@gmail.com>
2025-03-25 20:33:42 -0400

    Made the likes table

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,22 +1,28 @@
-import {varchar, pgTable, uuid} from "drizzle-orm/pg-core"
+    import {varchar, pgTable, uuid} from "drizzle-orm/pg-core"
 
 
-export const users = pgTable('users', {
-    user_id : uuid().primaryKey().defaultRandom(),
-    firstName : varchar({length : 70}).notNull(),
-    lastName : varchar({length : 70}).notNull(),
-    username : varchar({length : 70}).notNull(),
-    email : varchar({length : 70}).notNull().unique()
-});
+    export const users = pgTable('users', {
+        user_id : uuid().primaryKey().defaultRandom(),
+        firstName : varchar({length : 70}).notNull(),
+        lastName : varchar({length : 70}).notNull(),
+        username : varchar({length : 70}).notNull(),
+        email : varchar({length : 70}).notNull().unique()
+    });
 
 
-export type User = typeof users.$inferSelect;     //inferSelect helps in using SELECT queries for database. 
-export type NewUser = typeof users.$inferInsert;  //helps in inserting data into database & omits primary key or optional columns
+    export type User = typeof users.$inferSelect;     //inferSelect helps in using SELECT queries for database. 
+    export type NewUser = typeof users.$inferInsert;  //helps in inserting data into database & omits primary key or optional columns
 
 
-export const habits = pgTable('habits', {
-    id: uuid().primaryKey().defaultRandom(),
-    owner_id: uuid().notNull().references(() => users.user_id), // Foreign key referencing users
-    content: varchar({ length: 255 }), // Text content for the habit
-    image_url: varchar({ length: 255 })
-});
+    export const habits = pgTable('habits', {
+        id: uuid().primaryKey().defaultRandom(),
+        owner_id: uuid().notNull().references(() => users.user_id), // Foreign key referencing users
+        content: varchar({ length: 255 }), // Text content for the habit
+        image_url: varchar({ length: 255 })
+    });
+
+    export const likes = pgTable('likes', {
+        id: uuid().primaryKey().defaultRandom(),
+        owner_id: uuid().notNull().references(() => users.user_id),
+        habit_id: uuid().notNull().references(() => habits.id)
+    });
