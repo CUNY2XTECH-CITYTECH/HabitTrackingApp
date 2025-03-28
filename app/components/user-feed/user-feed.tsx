ceb06431dd365dc5830d1c18aee93a97250fe3e9
savarakhan28 <savarakhan28@gmail.com>
2025-03-28 18:41:34 +0000

    Update feature/user-feed-component

--- app/components/user-feed/user-feed.tsx
+++ app/components/user-feed/user-feed.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import HabitComponent, { HabitInfo } from "@/app/components/habit/habit-component";
- 
+//fixed
 
 interface UserFeedProps {
   userId: string;
